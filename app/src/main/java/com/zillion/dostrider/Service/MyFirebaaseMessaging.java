5b8de09039b86a3b7983358d05d57ec37b444f7f
nabeelnazir163 <nabeelnazir163@yahoo.com>
2018-06-11 14:55:34 +0500

    11/06/2018 2:55

--- app/src/main/java/com/zillion/dostrider/Service/MyFirebaaseMessaging.java
+++ app/src/main/java/com/zillion/dostrider/Service/MyFirebaaseMessaging.java
@@ -1,15 +1,28 @@
 package com.zillion.dostrider.Service;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+
 public class MyFirebaaseMessaging extends FirebaseMessagingService {
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
+    public void onMessageReceived(final RemoteMessage remoteMessage) {
 
         //Because this is rider app we havnt coding for this app
+
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MyFirebaaseMessaging.this, "" + remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
